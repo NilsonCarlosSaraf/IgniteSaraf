7c24e14de15d0856e7acecb7fadbb943643fdf46
NilsonCarlosSaraf <sarafnilson@gmail.com>
2023-08-15 16:27:40 -0300

    Header component included

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,15 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import './global.css';
 
 export function App() {
 
   return (
-    <Post author=" Diego Fernandes" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum magni perspiciatis laudantium cumque id atque similique consectetur nam distinctio recusandae aliquid, vero architecto quis dolorum aspernatur dolor in soluta pariatur." />
+    <div>
+      <Header />
+      <Post author=" Diego Fernandes" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum magni perspiciatis laudantium cumque id atque similique consectetur nam distinctio recusandae aliquid, vero architecto quis dolorum aspernatur dolor in soluta pariatur." />
+    </div>
 
   )
 }
