6733a477b07216533738f6afd03bde5ff6039f7c
NilsonCarlosSaraf <sarafnilson@gmail.com>
2023-08-16 21:46:15 -0300

    today class

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,27 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
+import styles from './App.module.css';
+
 import './global.css';
+
+
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post author=" Diego Fernandes" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum magni perspiciatis laudantium cumque id atque similique consectetur nam distinctio recusandae aliquid, vero architecto quis dolorum aspernatur dolor in soluta pariatur." />
+      
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+        <Post author=" Diego Fernandes" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum magni perspiciatis laudantium cumque id atque similique consectetur nam distinctio recusandae aliquid, vero architecto quis dolorum aspernatur dolor in soluta pariatur." />
+        <Post author="Gabriel Buzzi" content="A new post" />
+        </main>
+      </div>
     </div>
 
   )
