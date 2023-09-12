a727c640258da85e54df1a83fc18194ade6c7457
NilsonCarlosSaraf <sarafnilson@gmail.com>
2023-09-12 20:02:56 -0300

    todays work

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,9 @@
+import styles from './Comment.module.css'
+
+export default function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="" alt="" />
+        </div>
+    )
+}
