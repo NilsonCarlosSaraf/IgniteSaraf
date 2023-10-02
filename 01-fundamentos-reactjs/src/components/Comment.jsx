104230f6364b0adb66e1670f5978a90e792d595e
NilsonCarlosSaraf <sarafnilson@gmail.com>
2023-10-02 19:09:21 -0300

    todays study

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,9 +1,33 @@
-import styles from './Comment.module.css'
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import styles from './Comment.module.css';
 
-export default function Comment() {
+export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="" alt="" />
+            <img src="https://img.freepik.com/fotos-premium/duck-in-azuki-nft-street-wear-pure-profile_899449-2218.jpg?w=826" alt="" />
+            
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Diego Fernandes</strong>
+                            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Delete comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
         </div>
     )
 }
