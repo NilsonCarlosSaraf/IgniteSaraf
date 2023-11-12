f6f65516f9c85dcff763743b5e7c0ac3abbb8506
NilsonCarlosSaraf <sarafnilson@gmail.com>
2023-11-12 11:13:57 -0300

    update branch

--- desafio01/DESAFIO/src/components/AddTask.tsx
+++ desafio01/DESAFIO/src/components/AddTask.tsx
@@ -1,11 +1,22 @@
 import { PlusCircle } from "@phosphor-icons/react";
 import styles from "./AddTask.module.css"
+import React, { useState } from "react";
 
 
-export function AddTask() {
+export function AddTask({ }) {
+    const [task, setTask] = useState('')
+
+    function onAddTask(e: React.ChangeEvent<HTMLInputElement>) {
+
+        setTask(e.target.value)
+        console.log(task)
+
+
+    }
+
     return (
         <form action="submit" className={styles.formAdd}>
-            <div className={styles.divAdd}><input type="text" placeholder="Adicione uma nova tarefa" className={styles.inputAdd} /><button className={styles.buttonAdd}>Criar<PlusCircle weight="bold" size={18}/></button></div>
+            <div className={styles.divAdd}><input onChange={(e) => onAddTask(e)} type="text" placeholder="Adicione uma nova tarefa" className={styles.inputAdd} /><button className={styles.buttonAdd}>Criar<PlusCircle weight="bold" size={18} /></button></div>
         </form>
     )
 }
