152f151fa14bf8c2217de05e08eaf1d4409c7a5b
NilsonCarlosSaraf <sarafnilson@gmail.com>
2023-11-13 06:22:30 -0300

    update branch

--- desafio01/DESAFIO/src/App.tsx
+++ desafio01/DESAFIO/src/App.tsx
@@ -1,22 +1,9 @@
-import { AddTask } from "./components/AddTask";
-
-
-import styles from './App.module.css';
-
-import './global.css';
-import { AppTitle } from "./components/AppTitle";
-import { CreateTask } from "./components/CreateTask";
-
 function App() {
 
   return (
-    <body>
-      <header className={styles.styleHeader}>
-        <AppTitle />
-        <AddTask />
-      </header>
-      <CreateTask />
-    </body>
+    <div>
+      <p>Hello World</p>
+    </div>
   )
 }
 
