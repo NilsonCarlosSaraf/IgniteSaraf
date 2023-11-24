058082ec2cd0c7d96ef4d07c476acc34b97f4dc5
NilsonCarlosSaraf <sarafnilson@gmail.com>
2023-11-24 06:58:38 -0300

    update branch

--- desafio01/DESAFIO/src/components/TaskCard.tsx
+++ desafio01/DESAFIO/src/components/TaskCard.tsx
@@ -15,9 +15,11 @@ export function TaskCard() {
 
     return (
         <div className={style.taskCard}>
-            {taskStatus ? <div className={style.taskCheckBoxUntoggled} onClick={handleTaskStatus} /> : <div className={style.taskCheckBoxToggled} onClick={handleTaskStatus}><Check /></div>}
+            {taskStatus ? <div className={style.taskCheckBoxUntoggled} onClick={handleTaskStatus} /> : <div className={style.taskCheckBoxToggled} onClick={handleTaskStatus}><Check color="white" /></div>}
             <div style={taskStatus ? { textDecoration: 'none' } : { textDecoration: 'line-through' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem non iure veniam. Debitis consectetur maiores exercitationem sapiente, neque dolores soluta id aut ex sed deserunt harum ipsum quia, accusantium fuga.</div>
-            <div><Trash color='var(--gray-300)' /></div>
+            <button>
+                <Trash size={16} />
+            </button>
         </div>
     )
 }
--- desafio01/DESAFIO/src/components/TaskSection.tsx
+++ desafio01/DESAFIO/src/components/TaskSection.tsx
@@ -3,11 +3,13 @@ import styles from './TaskSection.module.css'
 import { TaskCard } from './TaskCard'
 
 export function TaskSection() {
+
+
     return (
         <div className={styles.taskList}>
             <div className={styles.taskCounterContainer}>
                 <div className={styles.taskCounter1}>Tarefas Criadas <div>0</div></div>
-                <div className={styles.taskCounter2}>Concluídas <div>0</div></div>
+                <div className={styles.taskCounter2}>Concluídas <div>2 de 5</div></div>
             </div>
             <div className={styles.tasksArea}>
                 <ClipboardText color={'var(--gray-400)'} size={56} />
