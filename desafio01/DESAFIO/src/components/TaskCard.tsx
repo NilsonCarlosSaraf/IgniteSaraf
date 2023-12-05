0d8b2590b4a8ae72cf5231ad70637c2e03e445c9
NilsonCarlosSaraf <sarafnilson@gmail.com>
2023-12-05 07:11:44 -0300

    update branch

--- desafio01/DESAFIO/src/components/TaskCard.tsx
+++ desafio01/DESAFIO/src/components/TaskCard.tsx
@@ -2,24 +2,26 @@ import { Check, Trash } from "@phosphor-icons/react"
 import style from "./TaskCard.module.css"
 import { useState } from "react"
 
-export function TaskCard() {
+interface IProps {
+    content: string,
+}
+
+export function TaskCard({ content }: IProps) {
     function handleTaskStatus() {
-        if (taskStatus) {
-            setTaskStatus(false)
-        } else {
-            setTaskStatus(true)
-        }
+        setTaskStatus(state => !state)
     }
 
     const [taskStatus, setTaskStatus] = useState<boolean>()
 
     return (
         <div className={style.taskCard}>
-            {taskStatus ? <div className={style.taskCheckBoxUntoggled} onClick={handleTaskStatus} /> : <div className={style.taskCheckBoxToggled} onClick={handleTaskStatus}><Check color="white" /></div>}
-            <div style={taskStatus ? { textDecoration: 'none' } : { textDecoration: 'line-through' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem non iure veniam. Debitis consectetur maiores exercitationem sapiente, neque dolores soluta id aut ex sed deserunt harum ipsum quia, accusantium fuga.</div>
-            <button>
-                <Trash size={16} />
-            </button>
-        </div>
+            {taskStatus ? <div className={style.taskCheckBoxUntoggled} onClick={handleTaskStatus} /> : <div className={style.taskCheckBoxToggled} onClick={handleTaskStatus}><Check size={9} color="white" /></div>}
+            <div style={taskStatus ? { textDecoration: 'none', textAlign: 'start' } : { textDecoration: 'line-through', textAlign: 'start' }}>{content}</div>
+            <div>
+                <button>
+                    <Trash size={16} />
+                </button>
+            </div>
+        </div >
     )
 }
--- desafio01/DESAFIO/src/components/TaskSection.tsx
+++ desafio01/DESAFIO/src/components/TaskSection.tsx
@@ -3,7 +3,21 @@ import styles from './TaskSection.module.css'
 import { TaskCard } from './TaskCard'
 
 export function TaskSection() {
+    const taskList = [
+        {
+            text: "Ama e faz o que quiseres. Se calares, calarás com amor; se gritares, gritarás com amor; se corrigires, corrigirás com amor; se perdoares, perdoarás com amor. Se tiveres o amor enraizado em ti, nenhuma coisa senão o amor serão os teus frutos.",
+            id: 0
+        },
+        {
+            text: "A medida do amor é amar sem medida.",
+            id: 1
+        },
+        {
+            text: "A beleza é realmente um bom dom de Deus; mas que os bons não pensem que ela é um grande bem, pois Deus a distribui mesmo para os maus.",
+            id: 2
+        },
 
+    ]
 
     return (
         <div className={styles.taskList}>
@@ -12,12 +26,15 @@ export function TaskSection() {
                 <div className={styles.taskCounter2}>Concluídas <div>2 de 5</div></div>
             </div>
             <div className={styles.tasksArea}>
-                <ClipboardText color={'var(--gray-400)'} size={56} />
-                <div>
-                    <p>Você ainda não tem tarefas cadastradas</p>
-                    <p>Crie tarefas e organize seus itens</p>
-                </div>
-                <TaskCard />
+                {taskList.length != 0 ?
+                    taskList.map((task) => <TaskCard content={task.text} />)
+                    : <div>
+                        <ClipboardText color={'var(--gray-400)'} size={56} />
+                        <div>
+                            <p>Você ainda não tem tarefas cadastradas</p>
+                            <p>Crie tarefas e organize seus itens</p>
+                        </div>
+                    </div>}
             </div>
         </div >
     )
