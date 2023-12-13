b28cdcfa9cbe91c8f5c3dd00c18fbcc98b7d383d
NilsonCarlosSaraf <sarafnilson@gmail.com>
2023-12-13 11:56:25 -0300

    update branch

--- desafio01/DESAFIO/src/App.tsx
+++ desafio01/DESAFIO/src/App.tsx
@@ -3,19 +3,43 @@ import styles from './App.module.css';
 import { TaskSection } from './components/TaskSection';
 import './global.css';
 import { Logo } from './components/Logo';
+import { useState } from 'react';
 
 function App() {
+
+  const [task, setTask] = useState('')
+
+  function handleNewTask() {
+    setTask(event?.target.value)
+    console.log('task:', task)
+  }
+
+  const [newText, setNewText] = useState([
+    'teste 1',
+    'teste 2'])
+
+  function onHandleSubmit() {
+    event?.preventDefault()
+
+
+
+    setNewText([...newText, task])
+    setTask('')
+
+    console.log("ARRAY:", newText)
+  }
+
 
   return (
     <div className={styles.appWrapper}>
       <header className={styles.appHeader}>
         <Logo />
-        <div className={styles.appInputSection}>
-          <input type="text" placeholder='Adicione uma nova tarefa' />
+        <form onSubmit={onHandleSubmit} className={styles.appInputSection}>
+          <input type="text" name='text1' value={task} onChange={handleNewTask} placeholder='Adicione uma nova tarefa' />
           <button> Criar
             <PlusCircle size={20} />
           </button>
-        </div>
+        </form>
       </header>
       <main className={styles.appMainSenction}>
         <TaskSection />
